a100951b1cb08cc7d3ab5c63f6423d3f25848f64
Nileshp8308 <nileshp8308@gmail.com>
2022-12-27 12:26:46 +0530

    javascrpt code

---
+++ Code about javascript/Else If, logical ampersant operator .js
@@ -0,0 +1,19 @@
+let a=10;
+let b=20;
+console.log(b)
+function abc(){
+    let c=50;
+    let d=60;
+    let e=70;
+    let f=40;
+    if(50<40){
+        console.log("Third class")
+    }
+    else if(60<40 && 60<50){
+        console.log("Second Class")
+    }
+    else if(70>40 && 70>60){
+        console.log("First Class")
+    }
+}
+
---
+++ Code about javascript/concantinate.js
@@ -0,0 +1,41 @@
+let a=5;
+let b=10;
+let name="Nilesh";
+console.log(a)
+console.log(a+b)
+console.log('a'+b)
+console.log(a+'a')
+console.log('a',a+b)
+console.log("a"+a)
+console.log('a',a+name)
+console.log('a'+a)
+console.log("a"+a)
+console.log('a' + a)
+console.log(a + b)
+console.log('a'+b-a)
+console.log(a,b)
+console.log(a+b+name)
+console.log(name+a+b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
